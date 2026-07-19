36d303ee0d1786c6db18d2a5d82b9ae4a10af76b
Debshuvra Sarkar <debshuvra2005@gmail.com>
2026-07-20 00:57:39 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Debshuvra Sarkar",
+    author_email="debshuvra2005@gmail.com",
+    packages=find_packages()
+)
